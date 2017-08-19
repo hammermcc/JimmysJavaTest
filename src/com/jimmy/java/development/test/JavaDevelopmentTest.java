1c65f54ea1343dab95af8b801b9d23b4b995c7b7
Jimmy McConalogue <hammermcc@hotmail.co.uk>
2017-08-19 09:14:52 +0100

    Step One

---
+++ src/com/jimmy/java/development/test/JavaDevelopmentTest.java
@@ -0,0 +1,31 @@
+package com.jimmy.java.development.test;
+
+public class JavaDevelopmentTest {
+
+	private static final int MAX_LOOP_COUNT = 100;
+	private static final int FIRST_DIVISOR  = 3;
+	private static final int SECOND_DIVISOR = 5;
+	
+	public static void main (String[] args) throws java.lang.Exception {
+		String textToWrite = null;
+		for (int i = 1; i <= MAX_LOOP_COUNT; i++) {
+			textToWrite = textToOutput(i);
+			if (i == MAX_LOOP_COUNT) {
+				textToWrite = textToWrite.substring(0, textToWrite.length()-1);
+			}
+			System.out.print(textToWrite);
+		}
+	}
+	
+	private static String textToOutput (int i) {
+		String outputText = "" + i;
+		if ((i % FIRST_DIVISOR == 0) && (i % SECOND_DIVISOR == 0)) {
+			outputText = "BaroSello";
+		} else if (i % FIRST_DIVISOR == 0) {
+			outputText = "Baro";
+		} else if (i % SECOND_DIVISOR == 0) {
+			outputText = "Sello";
+		}
+		return outputText + ",";
+	}
+}
