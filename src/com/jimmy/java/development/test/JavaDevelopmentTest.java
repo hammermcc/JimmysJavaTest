f012805923beb1138c0e9db87bcff8e41e0ff24e
Jimmy McConalogue <hammermcc@hotmail.co.uk>
2017-08-20 09:23:21 +0100

    Step Three

--- src/com/jimmy/java/development/test/JavaDevelopmentTest.java
+++ src/com/jimmy/java/development/test/JavaDevelopmentTest.java
@@ -1,14 +1,28 @@
 package com.jimmy.java.development.test;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class JavaDevelopmentTest {
 
-	private static final int MAX_LOOP_COUNT = 100;
-	private static final int FIRST_DIVISOR  = 3;
-	private static final int SECOND_DIVISOR = 5;
-	private static final int THIRD_DIVISOR  = 7;
+	private static final int    MAX_LOOP_COUNT = 100;
+	private static final int    FIRST_DIVISOR  = 3;
+	private static final int    SECOND_DIVISOR = 5;
+	private static final int    THIRD_DIVISOR  = 7;
+	
+	public  static final String ANSI_RESET     = "\033[0m";
+	public  static final String ANSI_GREEN     = "\033[32m";
+	public  static final String ANSI_RED       = "\033[31m";
+	public  static final String ANSI_CYAN      = "\033[36m";
+	public  static final String ANSI_BLUE      = "\033[34m";
 	
 	public static void main (String[] args) throws java.lang.Exception {
 		String textToWrite = null;
+		FileOutputStream fis = new FileOutputStream(new File("Javadevelopment.log"));
+		PrintStream out = new PrintStream(fis, true, "US-ASCII"); 
+		System.setOut(out);
+		
 		for (int i = 1; i <= MAX_LOOP_COUNT; i++) {
 			textToWrite = textToOutput(i);
 			if (i == MAX_LOOP_COUNT) {
@@ -21,13 +35,13 @@ public class JavaDevelopmentTest {
 	public static String textToOutput (int i) {
 		String outputText = "" + i;
 		if ((i % FIRST_DIVISOR == 0) && (i % SECOND_DIVISOR == 0)) {
-			outputText = "BaroSello";
+			outputText = ANSI_BLUE + "BaroSello" + ANSI_RESET;
 		} else if (i % THIRD_DIVISOR == 0) {
-			outputText = "Nardo";
+			outputText = ANSI_CYAN + "Nardo" + ANSI_RESET;
 		} else if (i % FIRST_DIVISOR == 0) {
-			outputText = "Baro";
+			outputText = ANSI_GREEN + "Baro" + ANSI_RESET;
 		} else if (i % SECOND_DIVISOR == 0) {
-			outputText = "Sello";
+			outputText = ANSI_RED + "Sello" + ANSI_RESET;
 		}
 		return outputText + ",";
 	}
--- src/com/jimmy/java/development/unit/test/JavaDevelopmentTestTest.java
+++ src/com/jimmy/java/development/unit/test/JavaDevelopmentTestTest.java
@@ -9,25 +9,25 @@ public class JavaDevelopmentTestTest extends TestCase {
 	public void testBaro () {
 		String output = JavaDevelopmentTest.textToOutput(3);
 		
-		assertEquals("Passing the value 3 does not match", "Baro,", output);
+		assertEquals("Passing the value 3 does not match", JavaDevelopmentTest.ANSI_GREEN + "Baro" + JavaDevelopmentTest.ANSI_RESET + ",", output);
 	}
 	
 	public void testSello () {
 		String output = JavaDevelopmentTest.textToOutput(5);
 		
-		assertEquals("Passing the value 5 does not match", "Sello,", output);
+		assertEquals("Passing the value 5 does not match", JavaDevelopmentTest.ANSI_RED + "Sello" + JavaDevelopmentTest.ANSI_RESET + ",", output);
 	}
 	
 	public void testNardo () {
 		String output = JavaDevelopmentTest.textToOutput(7);
 		
-		assertEquals("Passing the value 7 does not match", "Nardo,", output);
+		assertEquals("Passing the value 7 does not match", JavaDevelopmentTest.ANSI_CYAN + "Nardo" + JavaDevelopmentTest.ANSI_RESET + ",", output);
 	}
 	
 	public void testBaroSello () {
 		String output = JavaDevelopmentTest.textToOutput(15);
 		
-		assertEquals("Passing the value 15 does not match", "BaroSello,", output);
+		assertEquals("Passing the value 15 does not match", JavaDevelopmentTest.ANSI_BLUE + "BaroSello" + JavaDevelopmentTest.ANSI_RESET + ",", output);
 	}
 
 }
