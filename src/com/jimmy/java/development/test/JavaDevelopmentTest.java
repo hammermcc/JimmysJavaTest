24029a8f90dc44af000e21e3aa65ba680569f89f
Jimmy McConalogue <hammermcc@hotmail.co.uk>
2017-08-21 01:14:43 +0100

    Step Five

--- src/com/jimmy/java/development/test/JavaDevelopmentTest.java
+++ src/com/jimmy/java/development/test/JavaDevelopmentTest.java
@@ -4,6 +4,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
+import twitter4j.DirectMessage;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class JavaDevelopmentTest {
 
 	private static final int    MAX_LOOP_COUNT = 100;
@@ -18,7 +24,8 @@ public class JavaDevelopmentTest {
 	public  static final String ANSI_BLUE      = "\033[34m";
 	
 	public static void main (String[] args) throws java.lang.Exception {
-		String textToWrite = null;
+		OutputDetails textToWrite    = null;
+		String        twitterMessage = "";
 		FileOutputStream fis = new FileOutputStream(new File("Javadevelopment.log"));
 		PrintStream out = new PrintStream(fis, true, "US-ASCII"); 
 		System.setOut(out);
@@ -26,23 +33,105 @@ public class JavaDevelopmentTest {
 		for (int i = 1; i <= MAX_LOOP_COUNT; i++) {
 			textToWrite = textToOutput(i);
 			if (i == MAX_LOOP_COUNT) {
-				textToWrite = textToWrite.substring(0, textToWrite.length()-1);
+				textToWrite.setOutputText(textToWrite.getOutputText().substring(0, textToWrite.getOutputText().length()-1));
 			}
-			System.out.print(textToWrite);
+			if (textToWrite.isWriteToConsole()) {
+				System.out.print(textToWrite.getOutputText());
+			}
+			if (textToWrite.isWriteToTwitter()) {
+				twitterMessage += textToWrite.getOutputText();
+			}
+		}
+		if (twitterMessage != null) {
+			writeMessageToTwitter(twitterMessage);
 		}
 	}
 	
-	public static String textToOutput (int i) {
-		String outputText = "" + i;
+	public static OutputDetails textToOutput (int i) {
+		String outputText      = "" + i;
+		boolean writeToConsole = false;
+		boolean writeToTwitter = false;
 		if ((i % FIRST_DIVISOR == 0) && (i % SECOND_DIVISOR == 0)) {
 			outputText = ANSI_BLUE + "BaroSello" + ANSI_RESET;
+			writeToConsole = true;
 		} else if (i % THIRD_DIVISOR == 0) {
-			//outputText = ANSI_CYAN + "Nardo" + ANSI_RESET;
+			outputText = ANSI_CYAN + "Nardo" + ANSI_RESET;
+			writeToConsole = true;
 		} else if (i % FIRST_DIVISOR == 0) {
-			//outputText = ANSI_GREEN + "Baro" + ANSI_RESET;
+			outputText = ANSI_GREEN + "Baro" + ANSI_RESET;
+			writeToTwitter = true;
 		} else if (i % SECOND_DIVISOR == 0) {
-			//outputText = ANSI_RED + "Sello" + ANSI_RESET;
+			outputText = ANSI_RED + "Sello" + ANSI_RESET;
+			writeToConsole = true;
+			writeToTwitter = true;
 		}
-		return outputText + ",";
+		outputText += ",";
+		return new OutputDetails(outputText, writeToConsole, writeToTwitter);
+	}
+	
+	public static boolean writeMessageToTwitter(String twitterMessage) {
+		boolean messageSent = false;
+		DirectMessage message = null;
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("QLZzwq5uc37CipB6fczdAYdty")
+		  .setOAuthConsumerSecret("fiQy2ww5atJgPMCA3TNt9tiYMRL7THyfUU879iwG3z2O9Vc6YO")
+		  .setOAuthAccessToken("146793688-coPS7krtEbTwShgiZguabMtUo9qYWa8Lyv24SOGv")
+		  .setOAuthAccessTokenSecret("S6U9uQcnJV2sopUEeWWtr5HUsazHx4JfWl8LeT6YNFtmq");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+
+		 try {
+			 message = twitter.sendDirectMessage("hammermcc", twitterMessage);
+	         System.out.println("Direct message successfully sent to " + message.getRecipientScreenName());
+	         messageSent = true;
+	     } catch (TwitterException te) {
+	         te.printStackTrace();
+	         System.out.println("Failed to send a direct message: " + te.getMessage());
+	     } finally {
+	    	 if (message != null) {
+	    		 message = null;
+	    	 }
+	    	 if (twitter != null) {
+	    		 twitter = null;
+	    	 }
+	     }
+		 return messageSent;
+	}
+	
+	public static class OutputDetails {
+		private String outputText = null;
+		private boolean writeToConsole = false;
+		private boolean writeToTwitter = false;
+
+		public OutputDetails (String outputText, boolean writeToConsole, boolean writeToTwitter) {
+			this.outputText     = outputText;
+			this.writeToConsole = writeToConsole;
+			this.writeToTwitter = writeToTwitter;
+		}
+		
+		public String getOutputText() {
+			return this.outputText;
+		}
+
+		public void setOutputText(String outputText) {
+			this.outputText = outputText;
+		}
+
+		public boolean isWriteToConsole() {
+			return this.writeToConsole;
+		}
+
+		public void setWriteToConsole(boolean writeToConsole) {
+			this.writeToConsole = writeToConsole;
+		}
+
+		public boolean isWriteToTwitter() {
+			return this.writeToTwitter;
+		}
+
+		public void setWriteToTwitter(boolean writeToTwitter) {
+			this.writeToTwitter = writeToTwitter;
+		}
 	}
 }
--- src/com/jimmy/java/development/unit/test/JavaDevelopmentTestTest.java
+++ src/com/jimmy/java/development/unit/test/JavaDevelopmentTestTest.java
@@ -1,33 +1,47 @@
 package com.jimmy.java.development.unit.test;
 
 import com.jimmy.java.development.test.JavaDevelopmentTest;
+import com.jimmy.java.development.test.JavaDevelopmentTest.OutputDetails;
 
 import junit.framework.TestCase;
 
 public class JavaDevelopmentTestTest extends TestCase {
 	
 	public void testBaro () {
-		String output = JavaDevelopmentTest.textToOutput(3);
+		OutputDetails output = JavaDevelopmentTest.textToOutput(3);
 		
-		assertEquals("Passing the value 3 does not match", "3,", output);
+		assertEquals("Passing the value 3 text to output does not match", JavaDevelopmentTest.ANSI_GREEN + "Baro" + JavaDevelopmentTest.ANSI_RESET + ",", output.getOutputText());
+		assertEquals("Passing the value 3 writing to Console does not match", false, output.isWriteToConsole());
+		assertEquals("Passing the value 3 writing to Twitter does not match", true, output.isWriteToTwitter());
 	}
 	
 	public void testSello () {
-		String output = JavaDevelopmentTest.textToOutput(5);
+		OutputDetails output = JavaDevelopmentTest.textToOutput(5);
 		
-		assertEquals("Passing the value 5 does not match", "5,", output);
+		assertEquals("Passing the value 5 text to output does not match", JavaDevelopmentTest.ANSI_RED + "Sello" + JavaDevelopmentTest.ANSI_RESET + ",", output.getOutputText());
+		assertEquals("Passing the value 5 writing to Console does not match", true, output.isWriteToConsole());
+		assertEquals("Passing the value 5 writing to Twitter does not match", true, output.isWriteToTwitter());
 	}
 	
 	public void testNardo () {
-		String output = JavaDevelopmentTest.textToOutput(7);
+		OutputDetails output = JavaDevelopmentTest.textToOutput(7);
 		
-		assertEquals("Passing the value 7 does not match", "7,", output);
+		assertEquals("Passing the value 7 text to output does not match", JavaDevelopmentTest.ANSI_CYAN + "Nardo" + JavaDevelopmentTest.ANSI_RESET + ",", output.getOutputText());
+		assertEquals("Passing the value 7 writing to Console does not match", true, output.isWriteToConsole());
+		assertEquals("Passing the value 7 writing to Twitter does not match", false, output.isWriteToTwitter());
 	}
 	
 	public void testBaroSello () {
-		String output = JavaDevelopmentTest.textToOutput(15);
+		OutputDetails output = JavaDevelopmentTest.textToOutput(15);
 		
-		assertEquals("Passing the value 15 does not match", JavaDevelopmentTest.ANSI_BLUE + "BaroSello" + JavaDevelopmentTest.ANSI_RESET + ",", output);
+		assertEquals("Passing the value 15 text to output does not match", JavaDevelopmentTest.ANSI_BLUE + "BaroSello" + JavaDevelopmentTest.ANSI_RESET + ",", output.getOutputText());
+		assertEquals("Passing the value 15 writing to Console does not match", true, output.isWriteToConsole());
+		assertEquals("Passing the value 15 writing to Twitter does not match", false, output.isWriteToTwitter());
 	}
 
+	public void testSendMessageToTwitter () {
+		boolean messageSent = JavaDevelopmentTest.writeMessageToTwitter("Why am I writing code in a pub on a Sunday ?");
+		
+		assertEquals("Message was not sent to Twitter", true, messageSent);
+	}
 }
